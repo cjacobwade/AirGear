cb1d45fc096f90c2f2128876828f456453afb125
Christopher Wade <cjacobwade@gmail.com>
2013-08-28 23:35:32 -0500

    Camera controls working :)

--- AirGear/Assets/Prefabs/Player/Player.cs
+++ AirGear/Assets/Prefabs/Player/Player.cs
@@ -16,14 +16,8 @@ public class Player : MonoBehaviour {
 	public int maxSpeed, jumpSpeed, maxGravity;
 	public float gravityRate;
 	float currentSpeed, ySpeed;
-	Vector3 moveDirection;
-	CharacterController cc, camController;
-	
-	#region CameraVars
-	public Vector2 rotateSpeed, camHeight, cameraRot;
-	public Vector3 cameraOffset; //Position relative to player
-	public GameObject cam;
-	#endregion
+	Vector3 moveDirection, euler;
+	CharacterController cc;
 
 	public enum playerState
 	{
@@ -38,7 +32,6 @@ public class Player : MonoBehaviour {
 	void Awake () 
 	{
 		cc = GetComponent<CharacterController>();
-		camController = cam.GetComponent<CharacterController>();
 		currentSpeed = maxSpeed;
 	}
 	
@@ -47,8 +40,7 @@ public class Player : MonoBehaviour {
 	{
 		PlayerInput();
 		Movement();
-		CameraControl();
-		//CameraMinMax();
+		euler = transform.eulerAngles;
 	}
 	
 	void PlayerInput()
@@ -56,15 +48,6 @@ public class Player : MonoBehaviour {
 		if(Input.GetButtonDown("Jump"))
 			if(cc.isGrounded) Jump ();
 
-	}
-	
-	void OnGUI()
-	{
-		if(!Screen.lockCursor)
-		{
-			if(GUI.Button(new Rect(0,0,Screen.width/12,Screen.width/25),"Lock Cursor"))
-				Screen.lockCursor = true;
-		}
 	}
 	
 	void Movement()
@@ -76,33 +59,16 @@ public class Player : MonoBehaviour {
 			else
 				ySpeed = maxGravity;
 		}
-		moveDirection = new Vector3(Input.GetAxis("Horizontal"), ySpeed, (Input.GetAxis("Vertical")));
+		moveDirection.y = 0;
+		moveDirection.Normalize();
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), ySpeed, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
 		cc.Move (moveDirection*maxSpeed*Time.deltaTime);
 	}
 	
 	void Jump()
 	{
 		ySpeed = jumpSpeed*Time.deltaTime;
-	}
-	
-	void CameraControl()//Controls camera view
-	{
-		cam.transform.position = transform.position + cameraOffset;
-		cameraRot.y += Input.GetAxis("Mouse X")*rotateSpeed.x;//Horizontal control
-		cameraRot.x += -Input.GetAxis("Mouse Y")*rotateSpeed.y;//Vertical control
-
-		//Keep rotations within -360 and 360
-		if(cameraRot.y > 360) cameraRot.y -= 360;
-		if(cameraRot.y < -360) cameraRot.y += 360;
-
-		//Regulate camera height
-		if (cameraRot.x < camHeight.x) cameraRot.x = camHeight.x;
-		if (cameraRot.x > camHeight.y) cameraRot.x = camHeight.y;
-
-		//Set camera rotation
-		//cam.transform.eulerAngles = cameraRot;
-		cam.transform.Rotate(new Vector3(cameraRot.x, cameraRot.y,0)*Time.deltaTime, Space.World);
-		//transform.eulerAngles = new Vector3(transform.eulerAngles.x, cameraRot.y ,transform.eulerAngles.z);
 	}
 
 }
---
+++ AirGear/Assets/Scripts/Camera.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+	
+	public Transform player;
+	public Vector2 rotateSpeed, verticalLimit;
+	Vector2 inputVector;
+	Vector3 offset;
+	Quaternion camRotation;
+	
+	// Use this for initialization
+	void Awake () 
+	{
+		offset = player.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+//		player.transform.Rotate(player.transform.eulerAngles.x,Input.GetAxis("Mouse X")*rotateSpeed.x,0);
+//		transform.RotateAround(player.transform.position,new Vector3(Input.GetAxis("Mouse Y")*rotateSpeed.y,0,0),rotateSpeed.y*Time.deltaTime);
+//		camRotation = Quaternion.Euler(0,player.transform.eulerAngles.y,0);
+//		transform.position = player.transform.position - (camRotation*offset);
+//		
+//		transform.LookAt(player.transform);
+		if(Mathf.Abs(Input.GetAxis("Camera X")) > 0.01)
+		inputVector.x += Input.GetAxis("Camera X")*rotateSpeed.x*Time.deltaTime;
+		if(Mathf.Abs(Input.GetAxis("Camera Y")) > 0.01)
+		inputVector.y += Input.GetAxis("Camera Y")*rotateSpeed.y*Time.deltaTime;
+		
+		inputVector.y = ClampAngle(inputVector.y,verticalLimit.x,verticalLimit.y);
+		Quaternion rotation = Quaternion.Euler(inputVector.y,inputVector.x,0);
+		Vector3 position = player.position - rotation*offset;
+		
+		transform.rotation = rotation;
+		player.transform.rotation = Quaternion.Euler(0,inputVector.x,0);
+		transform.position = position;
+		
+	}
+	
+	float ClampAngle(float angle, float min, float max)
+	{
+		if(angle < -360)
+			angle +=360;
+		if(angle > 360)
+			angle -= 360;
+		return Mathf.Clamp(angle,min,max);
+	}
+	
+	void OnGUI()
+	{
+		if(!Screen.lockCursor)
+		{
+			if(GUI.Button(new Rect(0,0,Screen.width/12,Screen.width/25),"Lock Cursor"))
+				Screen.lockCursor = true;
+		}
+	}
+}
